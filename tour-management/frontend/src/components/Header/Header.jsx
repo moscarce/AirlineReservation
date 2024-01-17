db17f08c513529df4e2a03a20796d366413c41c7
mscarce <oluwa14tobi@gmail.com>
2024-01-17 15:56:33 +0100

    started experience section

--- tour-management/frontend/src/components/Header/Header.jsx
+++ tour-management/frontend/src/components/Header/Header.jsx
@@ -12,7 +12,7 @@ const nav_links = [
   display: 'Home'
 },
 {
-  path:'#',
+  path:'/about',
   display: 'About'
 },
 {
@@ -34,7 +34,10 @@ const Header = () => {
                   {
                     nav_links.map((item, index) =>(
                       <li className="nav__item" key={index}>
-                        <NavLink to={item.path}>{item.display}</NavLink>
+                        <NavLink 
+                        to={item.path}
+                        className={navClass => navClass.isActive ? "active__link" : ""}
+                        >{item.display}</NavLink>
                       </li>
                     ))
                   }
